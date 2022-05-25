63ea2377ae18f59891ef74262fdaa17317ddab7f
Marinz7320 <marinzovko541@gmail.com>
2022-05-25 14:28:21 +0200

    updatean 5.2

---
+++ DZ5.2/DZ5_1/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DZ5_1.Models
+{
+    public partial class Genre
+    {
+        public string genre_id { get; set; }
+        public string  genre_name { get; set; }
+       
+    }
+}
